7da20923053ab3adc17a52f63191290ea72235a6
Naveed kaimkhani <naveedkaimkhami@gmail.com>
2022-08-16 00:29:42 +0500

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
     //      home: SignUpPage(),
     
-        home: UploadEvent(),
+        home: welcomePage(),
       ),
     );
   }
